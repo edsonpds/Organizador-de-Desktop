51a80304763d9ec4d61e3cb42e1f7fddaf40a10c
Edson Peres <123039196+edsonpds@users.noreply.github.com>
2024-08-13 20:01:03 -0300

    Add files via upload

---
+++ Organizador de Desk/organizador.py
@@ -0,0 +1,36 @@
+import os 
+import PyInstaller
+import tkinter
+from tkinter.filedialog import askdirectory
+
+caminho = askdirectory(title='Selecione uma pasta ')
+
+lista_arquivos = os.listdir(caminho)
+
+locais = {
+        "imagens": [".png",".jpg",".gif",".webp"],
+        "planilhas": [".xlsx"],
+    "documentos" : [".pdf",".docx",".ppt",".pbix","txt"],
+    "csv": [".csv"],
+    "video":[".mp4",".wmv",".avi",".mov",],
+    "atalho": [".lnk"],
+    "programas":[".exe"],
+    
+    
+} 
+
+for arquivo in lista_arquivos: 
+    nome, extensao = os.path.splitext(f"{caminho}/{arquivo}")
+    for pasta in locais:
+        if extensao in locais[pasta]:
+            if not os.path.exists(f"{caminho}/{pasta}"):
+                os.mkdir(f"{caminho}/{pasta}")
+            os.rename(f"{caminho}/{arquivo}", f"{caminho}/{pasta}/{arquivo}")
+            tkinter.messagebox.showinfo(title=None, message="concluído", options= None)
+           
+            
+
+            
+         
+            
+
